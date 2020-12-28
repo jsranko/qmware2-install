9857e9429a493baaec8ac0ce710dab93877bd043
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2020-12-28 18:00:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -48,6 +48,8 @@ if ! [ -f "/usr/local/bin/docker-compose" ]; then
    sudo dnf install curl -y
    sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
    sudo chmod +x /usr/local/bin/docker-compose
+   sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 fi
 
 mkdir db_data
